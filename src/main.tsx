8cd11b777485d2927b80f93d025c8f06143a84dd
Spa Pas <jboadi@spepas.com>
2025-07-12 00:36:36 +0200

    apm fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
+import { init } from '@elastic/apm-rum';
+init({
+  serviceName: 'Spepas Web App',
+  serverUrl: import.meta.env.VITE_ELASTIC_APM_SERVER,
+  serviceVersion: '0.0.1',
+  active: true,
+  breakdownMetrics: true,
+  distributedTracingOrigins: ['*']
+});
+
 //src/main.tsx
 import './index.css';
 
-import { init } from '@elastic/apm-rum';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
@@ -12,13 +21,6 @@ import { AuthProvider } from '@/features/auth';
 import { queryClient } from '@/lib';
 
 import App from './App.tsx';
-
-init({
-  serviceName: 'Spepas Web App',
-  serverUrl: import.meta.env.VITE_ELASTIC_APM_SERVER,
-  serviceVersion: '0.0.1',
-  active: true
-});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
